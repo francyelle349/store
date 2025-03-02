9d40c66d66395ebd3436e1b429cfa582db3338c1
francyelle349 <francyelle5849@gmail.com>
2025-03-01 21:24:33 -0400

    more config files

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db = db.getSiblingDB('database');
+
+db.createUser({
+    user: 'root',
+    pwd: 'root',
+    roles: [
+        {
+            role: 'readWrite',
+            db: 'database'
+        }
+    ]
+})
